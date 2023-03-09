e04320a9a71d663fd7873cca57143367e1dadf0f
Mahfod <mahfod.dev@gmail.com>
2023-03-09 15:47:33 +0100

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Navbar from './components/Navbar';
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,8 +12,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+		<html lang='en'>
+			<body>
+				<Navbar />
+				{children}
+			</body>
+		</html>
+  );
 }
